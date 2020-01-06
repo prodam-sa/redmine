52895202c082054cd12e89e7f6430ebfb7a03185
Hallison Batista <hallison.batista@prodam.am.gov.br>
2020-01-06 14:54:41 -0400

    Configuração do contexto

--- config/application.rb
+++ config/application.rb
@@ -19,6 +19,8 @@ Bundler.require(*Rails.groups)
 
 module RedmineApp
   class Application < Rails::Application
+    config.relative_url_root = '/projetos'
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
